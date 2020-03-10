c7e3a5055b8cff0f55ee5979b1191522997192a2
Bekir ÇELİK <bekircelik5858@gmail.com>
2020-03-10 10:20:11 +0300

    modal view added

--- ECommerce/Models/Product.cs
+++ ECommerce/Models/Product.cs
@@ -8,6 +8,7 @@ namespace ECommerce.Models
     public class Product
     {
         public int Id { get; set; }
+        public string ProductCode { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
